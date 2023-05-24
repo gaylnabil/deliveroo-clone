4fde39024a1666006c65636d5271bb57feff0d76
Nabil GAYL <gaylnabil@gmail.com>
2023-05-24 12:37:37 +0100

    start restaurants deliver project

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          // options={{ title: "Title" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,64 @@
+import { View, Text, Image, TextInput, ScrollView } from "react-native";
+import React, { FC, useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  ChevronDownIcon,
+  UserIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
+
+const HomeScreen: FC = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    // navigation.setOptions({ title: "Nabil" });
+    navigation.setOptions({ headerShown: false });
+  }, [navigation]);
+
+  return (
+    <SafeAreaView className="bg-white pt-1 pb-1">
+      {/* Header */}
+      <View className="flex-row pb-1 items-center mx-4 space-x-2">
+        <Image
+          source={{ uri: "https://links.papareact.com/wru" }}
+          className="w-7 h-7 bg-gray-300 rounded-full"
+        />
+
+        <View className="flex-1 ">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-lg">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 mx-4">
+        <View className="flex-row space-x-2 bg-gray-200 p-3 flex-1">
+          <MagnifyingGlassIcon size={20} color="gray" />
+          <TextInput
+            placeholder="Restaurants and Cuisines"
+            keyboardType="default"
+            className="flex-1"
+          />
+        </View>
+        <AdjustmentsVerticalIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* Body */}
+      <ScrollView className="bg-gray-100">
+        {/* Categories */}
+        <Categories />
+
+        {/* Features rows */}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
